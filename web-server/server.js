27023c2600b33ed25f493bfaa3fc4886d26bae7f
Ivan Matviichuk <imatviichuk@umass.edu>
2026-09-18 13:00:48 -0400

    we server

---
+++ web-server/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+const app = express();
+const PORT = 3000;
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => {
+  res.send('Hello, web!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
+});
