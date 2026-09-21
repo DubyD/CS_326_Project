309793e56f7a50e63974c0b96007f00f758b1a92
DubyD <140568071+DubyD@users.noreply.github.com>
2026-09-21 12:56:21 -0400

    separated methods
    in Server.js, I separated the in line methods from the paths for future editing, readability, and added comments

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
 import express from "express";
-const app = express();
-const PORT = 3000;
-app.set("view engine", "ejs");
-app.get("/", (req, res) => {
-  res.send("Hello, web!");
-});
 
-app.get("/about", (req, res) => {
+const app = express();
+const PORT = 3000; // This can be altered to a more preferable port if desired
+
+/**
+ * Handler methods
+ */
+
+const homePageHandler = (req, res) => {
+  res.send("Hello, web!");
+};
+
+const aboutPageHandler = (req, res) => {
   res.render("about", {
     title: "About",
     body: "We want to translate compiled and then decompiled code into C language. We utilized open sourced compilers and decompilers and are going to use Ai to rewrite the product into a human readable doc.",
   });
-});
+};
+
+/**
+ * Server paths, and actions
+ */
+
+app.set("view engine", "ejs");
+app.get("/", homePageHandler);
+
+app.get("/about", aboutPageHandler);
 
 app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`);
